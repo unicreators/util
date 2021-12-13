1731806820fe769310537fef6c54140bd63bfea4
unicreators <d.unicreators@gmail.com>
2021-12-13 12:29:15 +0800

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
 export declare const _isFunction: (value: any) => value is Function;
-export declare const _isNumber: (value: any) => value is Number;
-export declare const _isString: (value: any) => value is String;
+export declare const _isNumber: (value: any) => value is number;
+export declare const _isString: (value: any) => value is string;
 export declare const _isDate: (value: any) => value is Date;
 export declare const _isBoolean: (value: any) => boolean;
 export declare const _isObject: (value: any) => boolean;
--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@
 ///
 
 export const _isFunction = (value: any): value is Function => typeof value === 'function';
-export const _isNumber = (value: any): value is Number => typeof value === 'number';
-export const _isString = (value: any): value is String => typeof value === 'string';
+export const _isNumber = (value: any): value is number => typeof value === 'number';
+export const _isString = (value: any): value is string => typeof value === 'string';
 export const _isDate = (value: any): value is Date => value instanceof Date && !isNaN(value.getTime());
 export const _isBoolean = (value: any) => typeof value === 'boolean';
 export const _isObject = (value: any) => !_isNullOrUndefined(value) && typeof value === 'object' && !Array.isArray(value);
